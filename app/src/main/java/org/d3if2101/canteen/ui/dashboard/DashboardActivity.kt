c6933b57f135aa0d306decf55134921596440329
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-10-10 23:13:55 +0700

    refactor penjual module to library

--- app/src/main/java/org/d3if2101/canteen/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/org/d3if2101/canteen/ui/dashboard/DashboardActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavDestination
 import com.google.android.gms.common.internal.safeparcel.SafeParcelable
 import org.d3if2101.canteen.databinding.ActivityDashboardBinding
 import org.d3if2101.canteen.ui.menu.MenuActivity
+import org.d3if2101.canteenpenjual.MainActivity
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
@@ -20,7 +21,9 @@ class DashboardActivity : AppCompatActivity() {
             btnKantin1.setOnClickListener { navTo("kantin1") }
             btnKantin2.setOnClickListener { navTo("kantin2") }
             btnKantin3.setOnClickListener { navTo("kantin3") }
-            btnKantin4.setOnClickListener { navTo("kantin4") }
+            btnKantin4.setOnClickListener {
+                startActivity(Intent(this@DashboardActivity, MainActivity::class.java))
+            }
         }
     }
 
