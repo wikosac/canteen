577a9ed7d0dcb4a211e6ab1d221a8967eefa4d8d
Zeeus <airlanggapermana96@gmail.com>
2023-10-14 16:24:31 +0700

    Implement Add Product

--- app/src/main/java/org/d3if2101/canteen/datamodels/OrderHistoryItem.kt
+++ app/src/main/java/org/d3if2101/canteen/datamodels/OrderHistoryItem.kt
@@ -7,10 +7,16 @@ data class OrderHistoryItem(
     var orderStatus: String = "ORDER_STATUS",
     var orderPayment: String = "ORDER_PAYMENT",
     var price: String = "ORDER_PRICE",
+    var productIDs: List<OrderDetail> = listOf(OrderDetail("", 0)),
     var buyerUid: String = "BUYER_UID",
     var sellerUid: String = "SELLER_UID",
     var rating: Float = 5.0F,
     var review: String = "REVIEW",
     var productID: String = "PRODUCT_ID",
     var quantity: Int = 0
+)
+
+data class OrderDetail(
+    var productId: String = "",
+    var qtyOrder: Int = 0
 )
